5112240a4cdae63581beaee710f16f89870e9cd1
Will Barber <WillB97@users.noreply.github.com>
2022-11-13 01:41:34 +0000

    Refactor

--- april_vision/vision.py
+++ april_vision/vision.py
@@ -1,6 +1,6 @@
 from os import pathlike
 from pathlib import Path
-from typing import NamedTuple, Any, List
+from typing import NamedTuple, Any, List, Tuple
 
 import cv2
 import numpy as np
@@ -20,56 +20,35 @@ def _find_camera():
 # self._camera.set(cv2.CAP_PROP_FPS, 30)
 # self._camera.set(cv2.CAP_PROP_BUFFERSIZE, 1)
 
-class USBCamera:
-    def __init__(self, resolution, calibration):
-        self._camera = cv2.VideoCapture(0)  # TODO Detect this better
+
+class Camera:
+    def __init__(self, index: int, resolution: Tuple[int, int]) -> None:
+        self._camera = cv2.VideoCapture(index)
+        self._set_resolution(resolution)
 
         # Take and discard a camera capture
-        _ = self._camera.read()
+        _ = self._capture_single_frame()
 
-    def _set_camera_property(self, propery, value):
-        self._camera.set(propery, value)
-        actual = self._camera.get(propery)
+    def _set_camera_property(self, property: int, value: int) -> None:
+        self._camera.set(property, value)
+        actual = self._camera.get(property)
 
-        assert actual == value, (f"Failed to set property '{propery}', "
+        assert actual == value, (f"Failed to set property '{property}', "
                                  f"expected {value} got {actual}")
 
-    def set_resolution(self, resolution):
+    def _set_resolution(self, resolution: Tuple[int, int]) -> None:
         width, height = resolution
         self._set_camera_property(cv2.CAP_PROP_FRAME_WIDTH, width)
         self._set_camera_property(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
-    def get_resolution(self):
+    def _get_resolution(self) -> Tuple[int, int]:
         return (
             int(self._camera.get(cv2.CAP_PROP_FRAME_WIDTH)),
             int(self._camera.get(cv2.CAP_PROP_FRAME_HEIGHT)),
         )
 
-    def capture(self):
-        _ = self._camera.read()
-        ret, colour_frame = self._camera.read()
-
-        if not ret:
-            raise IOError("Capture from camera failed")
-
-        grey_frame = cv2.cvtColor(colour_frame, cv2.COLOR_BGR2GRAY)
-
-        return Frame(
-            grey_frame=grey_frame,
-            colour_frame=colour_frame
-        )
-
-    def close(self):
-        self._camera.release()
-
-
-class Camera:
-    def __init__(self, index: int) -> None:
-        self.video_device = cv2.VideoCapture(index)
-        pass
-
     def _capture_single_frame(self) -> np.ndarray:
-        ret, colour_frame = self.video_device.read()
+        ret, colour_frame = self._camera.read()
         if not ret:
             raise IOError("Failed to get frama from camera")
         return colour_frame
@@ -122,3 +101,6 @@ class Camera:
             corners=[marker.pixel_corners for marker in markers],
             ids=[marker.id for marker in markers])
         self._save(frame, name)
+
+    def close(self):
+        self._camera.release()
