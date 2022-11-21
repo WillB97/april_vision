693e7083cdc89eb8b52c4c16edbedd08d793d8b0
WillB97 <will@rudi.org.uk>
2022-11-21 22:24:47 +0000

    Sort out __all__

--- april_vision/__init__.py
+++ april_vision/__init__.py
@@ -2,3 +2,13 @@ from .marker import (CartesianCoordinates, Marker, Orientation,
                      PixelCoordinates, SphericalCoordinate)
 from ._version import __version__
 from .vision import Camera
+
+__all__ = [
+    '__version__',
+    'Camera',
+    'CartesianCoordinates',
+    'Marker',
+    'Orientation',
+    'PixelCoordinates',
+    'SphericalCoordinate',
+]
