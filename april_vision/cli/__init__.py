5a0caa0b8e434e3c9f9571235bdfc84080eab2d0
WillB97 <will@rudi.org.uk>
2022-12-31 19:41:21 +0000

    annotate_image command.

--- april_vision/cli/__init__.py
+++ april_vision/cli/__init__.py
@@ -1,6 +1,7 @@
 """The april_vision CLI."""
 import argparse
 import importlib
+import logging
 
 from april_vision._version import version
 
@@ -36,8 +37,24 @@ def build_argparser():
     return parser
 
 
+def setup_logger(debug=False):
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    # log from all loggers to stdout
+    root_logger = logging.getLogger()
+    root_logger.setLevel(logging.INFO)
+    root_logger.addHandler(console_handler)
+
+    if debug:
+        root_logger.setLevel(logging.DEBUG)
+
+
 def main():
     """CLI entry-point."""
+    setup_logger()
     parser = build_argparser()
     args = parser.parse_args()
 
--- april_vision/cli/annotate_image.py
+++ april_vision/cli/annotate_image.py
@@ -1,21 +1,75 @@
+"""
+Annotate an image file.
+
+Takes an input image, adds annotation and saves the image.
+"""
 import argparse
+import logging
+import os
+from pathlib import Path
 
-"""
-annotate_image:
-Takes an input image, adds annotation and saves the image
-"""
+import cv2
 
-# TODO impliment
+from ..frame_sources import ImageSource
+from ..marker import MarkerType
+from ..vision import Processor
+
+LOGGER = logging.getLogger(__name__)
 
 
 def main(args: argparse.Namespace):
-    print("Not Implimented - Annotate image")
+    """Annotate an image file using the provided args."""
+    input_file = Path(args.input_file)
+    if not input_file.exists():
+        LOGGER.fatal("Input file does not exist.")
+        return
+
+    source = ImageSource(args.input_file)
+    threads = os.cpu_count()
+    if threads is None:
+        threads = 4
+    processer = Processor(
+        source, threads=threads,
+        tag_family=args.tag_family.value, quad_decimate=args.quad_decimate,
+    )
+
+    frame = processer._capture()
+    markers = processer._detect(frame)
+    LOGGER.info(f"Found {len(markers)} markers.")
+    frame = processer._annotate(frame, markers)
+
+    # save frame
+    cv2.imwrite(args.output_file, frame.colour_frame)
+
+    if args.preview:
+        cv2.imshow('image', frame.colour_frame)
+        LOGGER.info("Press any key to close window.")
+        _ = cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 
 def create_subparser(subparsers: argparse._SubParsersAction):
-    parser = subparsers.add_parser("annotate_image")
+    """Annotate_image command parser."""
+    parser = subparsers.add_parser(
+        "annotate_image",
+        description="Annotate an image file with its markers",
+        help="Annotate an image file with its markers",
+    )
 
-    parser.add_argument("input_file", type=str)
-    parser.add_argument("output_file", type=str)
+    parser.add_argument("input_file", type=str, help="The image to process.")
+    parser.add_argument("output_file", type=str, help="The filepath to save the output to.")
+
+    parser.add_argument('--preview', action='store_true', help="Display the annotated image.")
+
+    parser.add_argument(
+        '--tag_family', type=MarkerType, default=MarkerType.APRILTAG_36H11,
+        choices=[marker.value for marker in MarkerType],
+        help="Set the marker family to detect, defaults to 'tag36h11'")
+    parser.add_argument(
+        '--quad_decimate', type=float, default=2,
+        help="Set the level of decimation used in the detection stage")
+
+    # parser.add_argument('--calibration', type=Path, default=None)
+    # parser.add_argument('--tag_sizes', default=None)
 
     parser.set_defaults(func=main)
