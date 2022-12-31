bc4971eb6922feee8de6977f14fa63bdb596baf7
WillB97 <will@rudi.org.uk>
2022-12-31 19:55:55 +0000

    Repair live command

--- april_vision/cli/live.py
+++ april_vision/cli/live.py
@@ -1,15 +1,18 @@
+"""
+Live camera demonstration.
+
+Opens the camera and does live marker detection.
+Can add overlays of fps, marker annotation, marker distance, etc..
+Option to save the current view of the camera.
+"""
 import argparse
+import os
 
 import cv2
 
-from april_vision import Camera
-
-"""
-live:
-Opens the camera and does live marker detection
-Can add overlays of fps, marker annotation, marker distance, etc..
-Option to save the current view of the camera
-"""
+from ..frame_sources import USBCamera
+from ..marker import MarkerType
+from ..vision import Processor
 
 # TODO add these functionality
 # live
@@ -20,7 +23,15 @@ Option to save the current view of the camera
 
 
 def main(args: argparse.Namespace):
-    cam = Camera(args.id, (1280, 720))  # TODO open camera in a smarter way
+    """Live camera demonstration."""
+    source = USBCamera(args.id, (1280, 720))  # TODO open camera in a smarter way
+    threads = os.cpu_count()
+    if threads is None:
+        threads = 4
+    cam = Processor(
+        source, threads=threads,
+        tag_family=args.tag_family.value, quad_decimate=args.quad_decimate,
+    )
 
     while True:
         frame = cam._capture()
@@ -37,10 +48,23 @@ def main(args: argparse.Namespace):
 
 
 def create_subparser(subparsers: argparse._SubParsersAction):
+    """Live command parser."""
     parser = subparsers.add_parser("live")
 
     parser.add_argument("--id", type=int, required=True)
-
     parser.add_argument("--annotate", action='store_true')
+    # parser.add_argument('--fps', action='store_true')
+
+    parser.add_argument(
+        '--tag_family', type=MarkerType, default=MarkerType.APRILTAG_36H11,
+        choices=[marker.value for marker in MarkerType],
+        help="Set the marker family to detect, defaults to 'tag36h11'")
+    parser.add_argument(
+        '--quad_decimate', type=float, default=2,
+        help="Set the level of decimation used in the detection stage")
+
+    # parser.add_argument('--calibration', type=Path, default=None)
+    # parser.add_argument('--tag_sizes', default=None)
+    # parser.add_argument('--distance', action='store_true')
 
     parser.set_defaults(func=main)
