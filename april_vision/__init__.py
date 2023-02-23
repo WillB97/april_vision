87c34b57f6a7444a1f178fcf75e7722e5aa31bba
JoshP <joshuadp117@gmail.com>
2023-02-23 22:54:55 +0000

    Add fix for cameras loading slow on windows https://github.com/opencv/opencv/issues/17687

--- april_vision/__init__.py
+++ april_vision/__init__.py
@@ -1,4 +1,7 @@
 """An AprilTags wrapper with camera discovery and axis conversion."""
+import os
+os.environ["OPENCV_VIDEOIO_MSMF_ENABLE_HW_TRANSFORMS"] = "0"
+
 from ._version import __version__
 from .calibrations import calibrations
 from .detect_cameras import find_cameras
