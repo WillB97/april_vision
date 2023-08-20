cf84209bcad2e0180e13e9fdc8ced8e4ff822cf1
WillB97 <will@rudi.org.uk>
2023-08-20 14:52:40 +0100

    Use locations.csv for test_data generation

--- tests/webots_generator/controllers/capture_robot/capture_robot.py
+++ tests/webots_generator/controllers/capture_robot/capture_robot.py
@@ -1,20 +1,57 @@
+import csv
 from math import tan
 from pathlib import Path
+from typing import NamedTuple
 
 from controller import Supervisor
-
-i = 0
 
 SAVE_PATH = Path(__file__).parents[3] / "test_data"
 
 
-def save(robot, camera):
-    global i
+class MarkerValues(NamedTuple):
+    index: int
+    distance: float
+    horizontal: float
+    vertical: float
+    theta: float
+    phi: float
+    r: float
+    yaw: float
+    pitch: float
+    roll: float
+    name: str = "marker"
+
+
+def load_test_values():
+    data = []
+    with open(SAVE_PATH / "locations.csv") as f:
+        csv_reader = csv.DictReader(f)
+        for row in csv_reader:
+            data.append(row)
+
+    return [
+        MarkerValues(
+                index=int(row["Index"]),
+                distance=float(row["distance"]),
+                horizontal=float(row["horizontal"]),
+                vertical=float(row["vertical"]),
+                theta=float(row["theta"]),
+                phi=float(row["phi"]),
+                r=float(row["r"]),
+                yaw=float(row["yaw"]),
+                pitch=float(row["pitch"]),
+                roll=float(row["roll"]),
+                name=row.get("name", "marker"),
+            )
+        for row in data
+    ]
+
+
+def save(robot, camera, i):
     robot.step(100)
     camera.saveImage(str(SAVE_PATH / f'img-{i:03d}.png'), 100)
     print(f"Saved image {i:03d}")
     robot.step(100)
-    i += 1
 
 
 def set_orientation(robot, yaw, pitch, roll):
@@ -42,82 +79,14 @@ def main():
     marker_transform = robot.getFromDef('marker_transform')
     marker_position = marker_transform.getField('translation')
 
-    set_orientation(robot, 0, 0, 0)
-
-    marker_position.setSFVec3f([2, 0.25, 0.25])
-    save(robot, camera)
-    marker_position.setSFVec3f([2, 0, 0.25])
-    save(robot, camera)
-    marker_position.setSFVec3f([2, -0.25, 0.25])
-    save(robot, camera)
-    marker_position.setSFVec3f([2, 0.25, 0])
-    save(robot, camera)
-    marker_position.setSFVec3f([2, 0, 0])
-    save(robot, camera)
-    marker_position.setSFVec3f([2, -0.25, 0])
-    save(robot, camera)
-    marker_position.setSFVec3f([2, 0.25, -0.25])
-    save(robot, camera)
-    marker_position.setSFVec3f([2, 0, -0.25])
-    save(robot, camera)
-    marker_position.setSFVec3f([2, -0.25, -0.25])
-    save(robot, camera)
-
-    marker_position.setSFVec3f([1, 0, 0])
-    set_orientation(robot, 0, 0, 0)
-    save(robot, camera)
-    set_orientation(robot, 0.7854, 0, 0)
-    save(robot, camera)
-    set_orientation(robot, -0.7854, 0, 0)
-    save(robot, camera)
-    set_orientation(robot, 0, 0.7854, 0)
-    save(robot, camera)
-    set_orientation(robot, 0, -0.7854, 0)
-    save(robot, camera)
-    set_orientation(robot, 0.7854, 0.7854, 0)
-    save(robot, camera)
-    set_orientation(robot, 0.7854, -0.7854, 0)
-    save(robot, camera)
-    set_orientation(robot, -0.7854, 0.7854, 0)
-    save(robot, camera)
-    set_orientation(robot, -0.7854, -0.7854, 0)
-    save(robot, camera)
-    set_orientation(robot, 0, 0, 0.7854)
-    save(robot, camera)
-    set_orientation(robot, 0.7854, 0, 0.7854)
-    save(robot, camera)
-    set_orientation(robot, -0.7854, 0, 0.7854)
-    save(robot, camera)
-    set_orientation(robot, 0, 0.7854, 0.7854)
-    save(robot, camera)
-    set_orientation(robot, 0, -0.7854, 0.7854)
-    save(robot, camera)
-    set_orientation(robot, 0.7854, 0.7854, 0.7854)
-    save(robot, camera)
-    set_orientation(robot, 0.7854, -0.7854, 0.7854)
-    save(robot, camera)
-    set_orientation(robot, -0.7854, 0.7854, 0.7854)
-    save(robot, camera)
-    set_orientation(robot, -0.7854, -0.7854, 0.7854)
-    save(robot, camera)
-    set_orientation(robot, 0, 0, -0.7854)
-    save(robot, camera)
-    set_orientation(robot, 0.7854, 0, -0.7854)
-    save(robot, camera)
-    set_orientation(robot, -0.7854, 0, -0.7854)
-    save(robot, camera)
-    set_orientation(robot, 0, 0.7854, -0.7854)
-    save(robot, camera)
-    set_orientation(robot, 0, -0.7854, -0.7854)
-    save(robot, camera)
-    set_orientation(robot, 0.7854, 0.7854, -0.7854)
-    save(robot, camera)
-    set_orientation(robot, 0.7854, -0.7854, -0.7854)
-    save(robot, camera)
-    set_orientation(robot, -0.7854, 0.7854, -0.7854)
-    save(robot, camera)
-    set_orientation(robot, -0.7854, -0.7854, -0.7854)
-    save(robot, camera)
+    for marker_test in load_test_values():
+        marker_position.setSFVec3f([
+            marker_test.distance,
+            marker_test.horizontal,
+            marker_test.vertical,
+        ])
+        set_orientation(robot, marker_test.yaw, marker_test.pitch, marker_test.roll)
+        save(robot, camera, marker_test.index)
 
 
 if __name__ == "__main__":
