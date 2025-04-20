785ad8567771af706bfd9e12637bb9b43e93c25a
WillB97 <will@rudi.org.uk>
2025-04-20 20:05:35 +0100

    Rework extra calibrations import

--- april_vision/calibrations/__init__.py
+++ april_vision/calibrations/__init__.py
@@ -1,5 +1,9 @@
 """Packaged calibration files."""
 import os
+from pathlib import Path
 
 calibrations = os.environ.get('OPENCV_CALIBRATIONS', '.').split(':')
-calibrations.append(os.path.dirname(__file__))
+calibrations.append(Path(__file__).parent)
+
+extra_calibrations = calibrations.copy()
+extra_calibrations.insert(1, Path(__file__).parent / 'extra_calibrations')
--- april_vision/calibrations/extra_calibrations/__init__.py
+++ april_vision/calibrations/extra_calibrations/__init__.py
@@ -1,5 +1,1 @@
-"""Packaged calibration files."""
-import os
-
-extra_calibrations = os.environ.get('OPENCV_CALIBRATIONS', '.').split(':')
-extra_calibrations.append(os.path.dirname(__file__))
+"""Packaged additional calibration files."""
